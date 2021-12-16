3eba8f52fbdd966150b433a755c898f54c66f76f
Almaz A <61569345+Mexatronik@users.noreply.github.com>
2021-12-16 14:58:07 -0500

    Update rectangle.py

--- Tracing ABCD/rectangle.py
+++ Tracing ABCD/rectangle.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
 		rospy.init_node('turtle5')
 		turtle = WritingTurtle('/turtle5/cmd_vel', '/turtle5/pose')
 		turtle.rectangle()
-		turtle.end()
+		
 	except rospy.ROSInterruptException:
 		pass
